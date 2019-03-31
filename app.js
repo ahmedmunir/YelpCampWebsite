abf443be8c21ff5069edc599692cdc9d09e64914
Ahmed Munir Mohammed <abomunir1993@outlook.com>
2019-03-31 05:52:07 +0000

    Add mlab database globally

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ app.use(function(req, res, next){
 });
 
 //DataBase Configuration
-mongoose.connect('mongodb://localhost/yelpcampv2', {useNewUrlParser: true, autoIndex: false});
+// mongoose.connect('mongodb://localhost/yelpcampv2', {useNewUrlParser: true, autoIndex: false});
+mongoose.connect("mongodb://drjempo:Brockliden20@ds229186.mlab.com:29186/yelpcamp", {useNewUrlParser: true, autoIndex: false});
 
 // Rest of configuration for user authentication
 passport.use(User.createStrategy());
