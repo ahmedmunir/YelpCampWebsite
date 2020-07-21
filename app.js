eb8de6ea90c4b0f268340a37c4398388edc82d7b
ahmedmunir <mryagamie@outlook.com>
2020-07-21 03:27:53 +0200

    update credentials

--- app.js
+++ app.js
@@ -46,9 +46,10 @@ app.use(function(req, res, next){
 
 //DataBase Configuration
 // mongoose.connect('mongodb://localhost/yelpcampv2', {useNewUrlParser: true, autoIndex: false});
-mongoose.connect("mongodb://drjempo:brockliden20@ds229186.mlab.com:29186/yelpcamp", {
+mongoose.connect("mongodb://drjempo:brockliden20@ds229186.mlab.com:29186/yelpcamp?authSource=yelpcamp&w=1", {
     useNewUrlParser: true, 
     autoIndex: false,
+    useUnifiedTopology: true
 });
  
 // Rest of configuration for user authentication
